3ddad3a6bc3f0fc26f8d586ee433727e5c055b5e
arl1275 <54734069+arl1275@users.noreply.github.com>
2024-11-08 23:06:47 -0600

    added services

--- src/routes/servicesheets.routes.ts
+++ src/routes/servicesheets.routes.ts
@@ -3,12 +3,16 @@ import { verificarToken } from "../middleware/middleware.user";
 import { createssheetcontroller } from "../controller/servicesheet.controller";
 const routershet = Router();
 
-routershet.get('/dsheet' );
+//delete Service Order
+routershet.get('/dsheet', verificarToken, );
 
+//Generate Service Order
 routershet.get('/gsheet', verificarToken, createssheetcontroller);
 
-routershet.post('/csheet');
+//Read Service Order
+routershet.post('/rsheet', verificarToken,);
 
-routershet.put('/upsheet');
+//Update Service Order
+routershet.put('/upsheet', verificarToken,);
 
 export default routershet;
